0916aa34ca0f50f6b0274ee6dce2534493b9dfe2
AnaModolo12 <ana.modolo.senai404@gmail.com>
2024-08-02 11:41:09 -0300

    Add files via upload

---
+++ PHP/index.php
@@ -0,0 +1,21 @@
+<?php
+$nome = 'modolo';
+$escola = 'SESI cerquilho';
+$cidade = 'cerquilho';
+$CE = '428';
+$curso = 'Desenvolvimento de sistemas';
+$media = 9.5;
+
+echo 'olá bem vindo ao SESI-SP <br>';
+
+echo "$nome <br>";
+echo "$escola <br>";
+echo "$cidade <br>";  
+echo "$CE <br>";
+echo "$curso <br>";
+echo "$media <br>"; 
+
+echo "olá $nome, seja bem vinda ao SESI de $cidade, sua nota no curso de $curso é: $media ";
+
+
+?>
